6514ed7a5571ccbb38d74fbc2fc5c6e4e8cfd56c
hana <h.sasaki.sys23@morijyobi.ac.jp>
2023-11-22 12:14:48 +0900

    Bmi診断の修正

--- Bmi.java
+++ Bmi.java
@@ -1,41 +1,42 @@
-import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-	for(int i = 0; i < 2; i++) {
-        String type = "";
-        System.out.println("BMIを計算して、肥満度を表示します。");
-        System.out.println("身長(m)を入力してください。");
-        double height = Double.parseDouble(scanner.nextLine());
-        System.out.println("体重(kg)を入力してください。");
-        double weight = Double.parseDouble(scanner.nextLine());
+    import java.util.Scanner;
 
-        double BMI = weight / (height * height);
+    public class Bmi {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            for(int i = 0; i < 2; i++) {
+                String type = "";
+                System.out.println("BMIを計算して、肥満度を表示します。");
+                System.out.println("身長(m)を入力してください。");
+                double height = Double.parseDouble(scanner.nextLine());
+                System.out.println("体重(kg)を入力してください。");
+                double weight = Double.parseDouble(scanner.nextLine());
 
-        if (BMI < 18.5) {
-            type = "やせ型";
-        } else if
-            (BMI < 25) {
-            type = "標準体重";
-        } else if
-            (BMI < 30) {
-            type = "肥満1度";
-            } else if
-        (BMI < 35) {
-            type = "肥満2度";
-        } else if
-        (BMI < 40) {
-            type = "肥満3度";
-        } else if
-        (BMI >= 40) {
-            type = "肥満4度";
-        }
+                double BMI = weight / (height * height);
+                String sv2 = String.format("%.2f",BMI);
 
-            System.out.println("あなたのBMIは" + BMI + "です。");
-            System.out.println("診断結果:" + type + "です。");
+                if (BMI < 18.5) {
+                    type = "やせ型";
+                } else if
+                (BMI < 25) {
+                    type = "標準体重";
+                } else if
+                (BMI < 30) {
+                    type = "肥満1度";
+                } else if
+                (BMI < 35) {
+                    type = "肥満2度";
+                } else if
+                (BMI < 40) {
+                    type = "肥満3度";
+                } else if
+                (BMI >= 40) {
+                    type = "肥満4度";
+                }
+
+
+                System.out.println("あなたのBMIは" + sv2 + "です。");
+                System.out.println("診断結果:" + type + "です。");
+            }
         }
     }
-
-        }
-
